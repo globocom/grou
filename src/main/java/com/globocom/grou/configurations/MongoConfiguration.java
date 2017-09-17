dc6a5d2a5de00ef0b57087c954d54961d25ec043
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-09-17 00:42:46 -0300

    Improve MongoConfiguration

--- src/main/java/com/globocom/grou/configurations/MongoConfiguration.java
+++ src/main/java/com/globocom/grou/configurations/MongoConfiguration.java
@@ -17,20 +17,42 @@
 package com.globocom.grou.configurations;
 
 import com.mongodb.Mongo;
-import com.mongodb.MongoClientURI;
-import org.springframework.context.annotation.Bean;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
-import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.singletonList;
+
 @Configuration
-public class MongoConfiguration {
+public class MongoConfiguration extends AbstractMongoConfiguration {
 
-    private static final String MONGO_URL = Optional.ofNullable(System.getenv("MONGO_URL")).orElse("mongodb://@localhost:27017/grou");
+    private static final String MONGO_HOST = Optional.ofNullable(System.getenv("MONGO_HOST")).orElse("localhost");
+    private static final String MONGO_PORT = Optional.ofNullable(System.getenv("MONGO_PORT")).orElse("27017");
+    private static final String MONGO_DB   = Optional.ofNullable(System.getenv("MONGO_DB")).orElse("grou");
+    private static final String MONGO_USER = System.getenv("MONGO_USER");
+    private static final String MONGO_PASS = System.getenv("MONGO_PASS");
 
-    @Bean
-    public Mongo mongo() throws UnknownHostException {
-        return Mongo.Holder.singleton().connect(new MongoClientURI(MONGO_URL));
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    protected String getDatabaseName() {
+        return MONGO_DB;
     }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        log.info(String.format("MONGO_HOST: %s, MONGO_PORT: %s, MONGO_DB: %s", MONGO_HOST, MONGO_PORT, MONGO_DB));
+        final List<MongoCredential> credentialsList = MONGO_USER == null || MONGO_PASS == null ? Collections.emptyList() :
+                singletonList(MongoCredential.createCredential(MONGO_USER, MONGO_DB, MONGO_PASS.toCharArray()));
+        return new MongoClient(singletonList(new ServerAddress(MONGO_HOST, Integer.valueOf(MONGO_PORT))), credentialsList);
+    }
+
 }
