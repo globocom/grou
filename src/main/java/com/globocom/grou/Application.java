2b1e3fd69e9e5769d275c2f0c9c3c3af82d45bd0
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-09-16 22:19:52 -0300

    Remove generic LoggingEventListener

--- src/main/java/com/globocom/grou/Application.java
+++ src/main/java/com/globocom/grou/Application.java
@@ -19,9 +19,7 @@ package com.globocom.grou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.core.mapping.event.LoggingEventListener;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -32,11 +30,6 @@ public class Application {
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public LoggingEventListener mongoEventListener() {
-        return new LoggingEventListener();
     }
 
 }
