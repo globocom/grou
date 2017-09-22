17a52d1ec3e533b38526ddeee4830e578861eb1b
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-09-22 18:46:00 -0300

    Suppress "EmptyMethod" warnings

--- src/test/java/com/globocom/grou/ApplicationTest.java
+++ src/test/java/com/globocom/grou/ApplicationTest.java
@@ -21,7 +21,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+@SuppressWarnings("EmptyMethod")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
