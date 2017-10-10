5c8f54a010242c2e9a7c0508fdb4745b0f47aeae
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-10-10 20:21:46 -0300

    Add Scheduling support

--- src/main/java/com/globocom/grou/Application.java
+++ src/main/java/com/globocom/grou/Application.java
@@ -20,11 +20,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableAutoConfiguration
+@EnableScheduling
 @Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
 public class Application {
 
