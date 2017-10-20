0d8c833c423a5cad59812bd90adeddd2bdb054c9
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-10-20 10:18:42 -0200

    Add report route (/report) with exclusive admin access only: initial implementation

---
+++ src/main/java/com/globocom/grou/controllers/ReportController.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017-2017 Globo.com
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globocom.grou.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/report")
+    public ResponseEntity<Void> report() {
+        // TODO
+        return ResponseEntity.ok().build();
+    }
+}
