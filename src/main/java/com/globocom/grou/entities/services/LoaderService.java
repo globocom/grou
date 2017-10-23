b73b90ef50f763080c9f85c532028d2d8a1ef9d4
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-10-23 04:10:11 -0200

    Using new Loader serialization (from json value) from redis

--- src/main/java/com/globocom/grou/entities/services/LoaderService.java
+++ src/main/java/com/globocom/grou/entities/services/LoaderService.java
@@ -1,15 +1,24 @@
 package com.globocom.grou.entities.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.globocom.grou.entities.Loader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 public class LoaderService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoaderService.class);
+
+    private final ObjectMapper mapper = new ObjectMapper();
 
     private final StringRedisTemplate redisTemplate;
 
@@ -20,20 +29,19 @@ public class LoaderService {
 
     public List<Loader> loaders() {
         return redisTemplate.keys("grou:loader:*").stream().map(k -> {
-            String loaderName = k.split(":")[2];
-            String loaderStatusStr = redisTemplate.opsForValue().get(k);
-            int idx;
-            String statusDetailed = "";
-            if ((idx = loaderStatusStr.indexOf(":")) > -1) {
-                statusDetailed = "Running test " + loaderStatusStr.substring(idx + 1);
-                loaderStatusStr = loaderStatusStr.substring(0, idx);
+            try {
+                String loaderStr = redisTemplate.opsForValue().get(k);
+                final Loader loader = mapper.readValue(loaderStr, Loader.class);
+                if (!loader.getStatusDetailed().isEmpty() && loader.getStatus() == Loader.Status.RUNNING) {
+                    String statusDetailed = "Running test " + loader.getStatusDetailed();
+                    loader.setStatusDetailed(statusDetailed);
+                }
+                return loader;
+            } catch (IOException e) {
+                LOGGER.error(e.getMessage(), e);
+                return null;
             }
-            Loader loader = new Loader();
-            loader.setName(loaderName);
-            loader.setStatus(Enum.valueOf(Loader.Status.class, loaderStatusStr));
-            loader.setStatusDetailed(statusDetailed);
-            return loader;
-        }).collect(Collectors.toList());
+        }).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
 }
