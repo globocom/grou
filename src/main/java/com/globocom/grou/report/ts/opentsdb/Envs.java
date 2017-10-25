5e6580425a54c42fb69e851ed0fbf1aa730a64a9
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-10-25 13:38:52 -0200

    Add *.opentsdb.Envs

---
+++ src/main/java/com/globocom/grou/report/ts/opentsdb/Envs.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017-2017 Globo.com
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globocom.grou.report.ts.opentsdb;
+
+import java.util.Optional;
+
+public enum Envs {
+
+    /**
+     * OpenTSDB URL
+     */
+    URL("URL", "http://localhost:4242");
+
+    /**
+     * Gets Env value.
+     *
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    private final String value;
+
+    Envs(String env, Object def) {
+        this.value = Optional.ofNullable(System.getenv(env)).orElse(String.valueOf(def));
+    }
+}
