3edb3c5d283fb86455fa664241aac7ebf1c55bbc
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-10-27 04:20:11 -0200

    Fix task scheduled "fixStatus"

--- src/main/java/com/globocom/grou/schedule/SchedulerService.java
+++ src/main/java/com/globocom/grou/schedule/SchedulerService.java
@@ -4,6 +4,8 @@ import com.globocom.grou.SystemEnv;
 import com.globocom.grou.entities.Loader;
 import com.globocom.grou.entities.Test;
 import com.globocom.grou.entities.repositories.TestRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,6 +20,8 @@ import java.util.stream.StreamSupport;
 @Service
 public class SchedulerService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SchedulerService.class);
+
     public static final int CONSUMER_PAUSE = Integer.parseInt(SystemEnv.CONSUMER_PAUSE.getValue());
     private final TestRepository testRepository;
     private Pageable pageable = new PageRequest(0, 99999);
@@ -29,7 +33,7 @@ public class SchedulerService {
 
     @Scheduled(fixedRate = 60000)
     public void fixStatus() {
-        Instant instante = Instant.now().minus((Integer.parseInt(SystemEnv.MAX_RETRY.getValue()) * CONSUMER_PAUSE), ChronoUnit.SECONDS);
+        Instant instante = Instant.now().minus(1 + (Integer.parseInt(SystemEnv.MAX_RETRY.getValue()) * (CONSUMER_PAUSE/1000)), ChronoUnit.SECONDS);
         Test.Status status = Test.Status.ENQUEUED;
         Page<Test> tests = testRepository.findByLastModifiedByBeforeAndStatus(instante, status, pageable);
         StreamSupport.stream(tests.spliterator(), false).forEach(t -> {
@@ -39,6 +43,7 @@ public class SchedulerService {
             loaderUndef.setStatusDetailed("Forgotten in the queue");
             t.setStatus(Test.Status.ERROR);
             testRepository.save(t);
+            LOGGER.warn("Test " + t.getProject() + "." + t.getName() + ": Forgotten in the queue. new status ERROR.");
         });
     }
 }
